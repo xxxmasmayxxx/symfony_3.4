4f1935ebd72e56eeba6e3106dc41993d4f910ee9
Andrey Majoroff <andrey.majoroff@gmail.com>
2018-12-14 20:48:48 +0200

    move templates to bundle & add news jpg files

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,11 +11,10 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+//      todo  getNewsCategories(list, 5, last)
+//
+        return $this->render('@App/default/index.html.twig');
     }
 }
